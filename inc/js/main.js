5c1d469640ef3eb51ef2f7db1dad1c3daa455897
Abu Taher Muhamamd <abut1081@gmail.com>
2024-01-29 02:09:30 +0600

    design updated

--- inc/js/main.js
+++ inc/js/main.js
@@ -1,59 +1,79 @@
-(function () {
-    const emblaNode = document.querySelector(".z_hero--slider");
-    const dotsNode = document.querySelector(".z_hero--slider--dots");
-    const options = {
-        loop: false,
-    };
+// AOS
+if (AOS) {
+  AOS.init({
+    easing: "ease-out",
+    useClassNames: false,
+    easing: "ease-in-out",
+    mirror: true,
+  });
+}
 
-    const embla = EmblaCarousel(emblaNode, options);
-    // addDotBtnsAndClickHandlers(embla);
-    const addDotBtnsAndClickHandlers = (emblaApi, dotsNode) => {
-        let dotNodes = [];
+// Carousel Implementation
+const slider = document.querySelector(".z_hero--slider");
+const sliderDots = document.querySelector(".z_hero--slider--dots");
+const options = {
+  loop: false,
+};
 
-        const addDotBtnsWithClickHandlers = () => {
-            dotsNode.innerHTML = emblaApi
-                .scrollSnapList()
-                .map(
-                    () =>
-                        '<button class="z_hero--slider--dot" type="button"></button>'
-                )
-                .join("");
+const embla = EmblaCarousel(slider, options);
+// addDotBtnsAndClickHandlers(embla);
+const addDotBtnsAndClickHandlers = (emblaApi, sliderDots) => {
+  let dotNodes = [];
 
-            dotNodes = Array.from(
-                dotsNode.querySelectorAll(".z_hero--slider--dot")
-            );
+  const addDotBtnsWithClickHandlers = () => {
+    sliderDots.innerHTML = emblaApi
+      .scrollSnapList()
+      .map(() => '<button class="z_hero--slider--dot" type="button"></button>')
+      .join("");
 
-            dotNodes.forEach((dotNode, index) => {
-                dotNode.addEventListener(
-                    "click",
-                    () => emblaApi.scrollTo(index),
-                    false
-                );
-            });
-        };
+    dotNodes = Array.from(sliderDots.querySelectorAll(".z_hero--slider--dot"));
 
-        const toggleDotBtnsActive = () => {
-            const previous = emblaApi.previousScrollSnap();
-            const selected = emblaApi.selectedScrollSnap();
-            dotNodes[previous].classList.remove("z_hero--slider--dot--selected");
-            dotNodes[selected].classList.add("z_hero--slider--dot--selected");
-        };
+    dotNodes.forEach((dotNode, index) => {
+      dotNode.addEventListener("click", () => emblaApi.scrollTo(index), false);
+    });
+  };
 
-        emblaApi
-            .on("init", addDotBtnsWithClickHandlers)
-            .on("reInit", addDotBtnsWithClickHandlers)
-            .on("init", toggleDotBtnsActive)
-            .on("reInit", toggleDotBtnsActive)
-            .on("select", toggleDotBtnsActive);
+  const toggleDotBtnsActive = () => {
+    const previous = emblaApi.previousScrollSnap();
+    const selected = emblaApi.selectedScrollSnap();
+    dotNodes[previous].classList.remove("z_hero--slider--dot--selected");
+    dotNodes[selected].classList.add("z_hero--slider--dot--selected");
+  };
 
-        return () => {
-            dotsNode.innerHTML = "";
-        };
-    };
+  emblaApi
+    .on("init", addDotBtnsWithClickHandlers)
+    .on("reInit", addDotBtnsWithClickHandlers)
+    .on("init", toggleDotBtnsActive)
+    .on("reInit", toggleDotBtnsActive)
+    .on("select", toggleDotBtnsActive);
 
-    addDotBtnsAndClickHandlers(embla, dotsNode);
-    embla.on("destroy", addDotBtnsAndClickHandlers(embla, dotsNode));
-    embla.slideNodes(); // Access API
-})();
+  return () => {
+    sliderDots.innerHTML = "";
+  };
+};
 
+addDotBtnsAndClickHandlers(embla, sliderDots);
+embla.on("destroy", addDotBtnsAndClickHandlers(embla, sliderDots));
+embla.slideNodes(); // Access API
 
+// Accordion
+var acc = document.querySelectorAll(".z_faq--accordion--item");
+
+acc.forEach((item) => {
+  //   Get title and body
+  const title = item.querySelector(".z_faq--accordion--header");
+  const body = item.querySelector(".z_faq--accordion--body");
+
+  //   Add event
+  title.addEventListener("click", () => {
+    //   Toggle class
+    body.classList.toggle("active");
+    //   Change icon
+    const icon = title.querySelector(".z_faq--accordion--icon");
+    if (body.classList.contains("active")) {
+      icon.src = "./assets/img/arrow-up.svg";
+    } else {
+      icon.src = "./assets/img/arrow.svg";
+    }
+  });
+});
